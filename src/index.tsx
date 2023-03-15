b52dd42276402a1526c8aea0a9cdaa75f3d2c9d1
goeun208 <qop341@gmail.com>
2023-03-16 01:00:26 +0900

    feat: qna 아코디언 메뉴 완성, 프로젝트 링크 연결

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,8 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <>
     <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </>
 );
