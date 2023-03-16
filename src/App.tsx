2d718be80ffecbd6b52f41e0d8aa25431ae1745d
Sangjun Park <sangjun1997@naver.com>
2023-03-17 00:33:07 +0900

    feat: 프로젝트 카드 이미지 lazy loading 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import styled from "styled-components";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Main, Projects, Recruit } from "pages";
+import { Main, Recruit } from "pages";
+
+// eslint-disable-next-line
+const Projects = lazy(() => import("./pages/Projects/ProjectsPage"));
 
 function App() {
   return (
     <Container>
       <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/recruit" element={<Recruit />} />
-        </Routes>
+        <Suspense fallback={<div>Loading ..</div>}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/recruit" element={<Recruit />} />
+          </Routes>
+        </Suspense>
       </Router>
     </Container>
   );
