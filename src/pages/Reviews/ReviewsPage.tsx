1814caf939d8b5f4a963a39eb73dfa1bccfac628
goeun208 <qop341@gmail.com>
2023-03-29 00:28:42 +0900

    chore: 코드 주석 수정

--- src/pages/Reviews/ReviewsPage.tsx
+++ src/pages/Reviews/ReviewsPage.tsx
@@ -235,7 +235,7 @@ const MobileReviewBoxContainer = styled.div`
   padding: 0px;
   width: 390px;
   flex-wrap: wrap; // 복수의 행
-  // margin-bottom: 60px;
+  margin-bottom: 60px;
 `;
 
 const MobileReviewBox = styled.div`
