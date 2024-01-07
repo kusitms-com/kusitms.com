a0e966d9faa8aa069a708a2ec0aadefec650b6df
Sangjun Park <sangjun1997@naver.com>
2024-01-07 10:36:46 +0900

    feat: 29기 운영진 리크루팅 수정

--- src/components/recruit/RecruitProcess.tsx
+++ src/components/recruit/RecruitProcess.tsx
@@ -22,26 +22,26 @@ interface processInfo {
   period: string;
 }
 
-const KUSITMS_YEAR = "28기 활동";
+const KUSITMS_YEAR = "29기 활동";
 const PROCESS_INFO = [
   {
     img: level1,
-    period: "7월 17일(월) ~ 8월 6일(일)\n자정 마감",
+    period: "1월 6일(토) ~ 1월 16일(화)\n오후 11:59 마감",
   },
   {
     img: level2,
-    period: "8월 14일(월)\n합격자/불합격자 전체 연락",
+    period: "1월 18일(목)\n합격자/불합격자 전체 연락",
   },
   {
     img: level3,
-    period: "8월 19일(토) ~ 8월 20일(일)\n서류 합격자 개별 안내",
+    period: "1월 20일(토) ~ 1월 21일(일)\n서류 합격자 개별 안내",
   },
   {
     img: level4,
-    period: "8월 22일(화)\n합격자/불합격자 전체 연락",
+    period: "1월 22일(월)\n합격자/불합격자 전체 연락",
   },
 ];
-const KUSITMS_PERIOD = "8월 26일(토) ~ 12월 2일(토)";
+const KUSITMS_PERIOD = "선발직후 ~ 6월 둘째 주 주말";
 
 const RecruitProcess = () => {
   const isMobile = useIsMobile();
