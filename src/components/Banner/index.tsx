a4ab578c78c759b96843791f8083bd374bced082
parkyejin <uiop5809@naver.com>
2024-01-22 20:42:55 +0900

    feat: 29기 학회원 리크루팅 수정

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable */
-import * as s from './styles';
+import * as s from "./styles";
 import LinkIcon from "../../images/common/LinkIcon.svg";
 
 const Banner = () => {
   const onClickBtn = () => {
     window.open(
-      "https://forms.gle/ssoBW9swWhqr2rhs6",
+      "https://docs.google.com/forms/d/e/1FAIpQLSfVjXycd48Ugg-Yjb9Srj5yZQU7hSTE575CncXCQqtRVJZh-Q/viewform",
       "_blank"
     );
   };
@@ -13,14 +13,14 @@ const Banner = () => {
   return (
     <s.Wrapper>
       <s.ContentBox>
-        <s.GuideMent>KUSITMS 29기 운영진 리크루팅 중</s.GuideMent>
+        <s.GuideMent>KUSITMS 29기 학회원 리크루팅 중</s.GuideMent>
         <s.LinkBtn onClick={onClickBtn}>
           지원 바로가기
-          <s.Linkicon src={LinkIcon} alt='link' />
+          <s.Linkicon src={LinkIcon} alt="link" />
         </s.LinkBtn>
       </s.ContentBox>
     </s.Wrapper>
-  )
+  );
 };
 
 export default Banner;
