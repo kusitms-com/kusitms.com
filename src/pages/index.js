98e3e898b34cac704d50a5a6bcb2e322fdc76a83
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-18 23:34:08 +0900

    remove: 기존 파일 전부 삭제

--- src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Main } from "./Main/MainPage";
-export { default as Projects } from "./Projects/ProjectsPage";
-export { default as Recruit } from "./Recruit/RecruitPage";
-export { default as Reviews } from "./Reviews/ReviewsPage";
