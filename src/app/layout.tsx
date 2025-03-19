1d3303eb1ab489830fe935b671818c3c12735aeb
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-19 23:59:46 +0900

    feat: eslint 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from "next";
 import "./globals.css";
 
 const geistSans = Geist({
