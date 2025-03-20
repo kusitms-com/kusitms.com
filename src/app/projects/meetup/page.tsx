86cd633bb567a27618dda63ad99d90264a804eae
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-03-20 21:29:12 +0900

    design : 인트로 컴포넌트 디자인

--- src/app/projects/meetup/page.tsx
+++ src/app/projects/meetup/page.tsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
 import React from "react";
+import ImageCard from "@/components/projects/ImageCard";
+import EventIntro from "@/components/projects/EventIntro";
 
 function MeetupPage() {
-  return <div>MeetupPage</div>;
+  return (
+    <section className="flex w-full max-w-[1180px] gap-[90px] mt-[100px] justify-center items-center">
+      <ImageCard>
+        <ImageCard.Sticker>
+          <Image
+            src="/projects/sticker/MeetupSticker.svg"
+            alt="스티커"
+            width={70}
+            height={100}
+            priority
+          />
+        </ImageCard.Sticker>
+        <ImageCard.Image src="/projects/tmp/meetup_tmpImg.jpeg" />
+      </ImageCard>
+      <EventIntro type="meetup" />
+    </section>
+  );
 }
 
 export default MeetupPage;
