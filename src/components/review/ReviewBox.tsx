e1156eea148e23ad705f768463ab6d26a3a474ea
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-04-09 21:39:48 +0900

    design: 스크롤 제거

--- src/components/review/ReviewBox.tsx
+++ src/components/review/ReviewBox.tsx
@@ -6,7 +6,7 @@ export const ReviewBox = (props: Review) => {
     <div className="px-8 py-12 max-w-[380px] max-h-[450px] bg-[#F2F2F8] rounded-[32px]">
       <h3 className="text-2xl font-extrabold mb-2">{props.name}</h3>
       <h4 className="text-xl font-normal mb-8 text-[#05f]">{props.team}</h4>
-      <p className="text-base font-normal max-h-[260px] overflow-y-auto scrollbar-custom">
+      <p className="text-base font-normal max-h-[260px] overflow-y-auto custom-scrollbar-none">
         {props.review}
       </p>
     </div>
