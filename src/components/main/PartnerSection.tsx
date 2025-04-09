7547913bdbc52157420ddfc50fe6f12a88558e99
유림 <yulimh001@gmail.com>
2025-04-10 02:21:21 +0900

    feat: 기획팀 요청사항에 따라 파트너사 컴포넌트 수정

--- src/components/main/PartnerSection.tsx
+++ src/components/main/PartnerSection.tsx
@@ -3,8 +3,34 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 const PartnerSection = () => {
+
+    const partners = [
+        {
+            name: "인프런",
+            description:
+                "IT기술 정보 플랫폼 ‘인프런'은 현재 3,000여개가 넘는 IT 분야 학습 콘텐츠를 보유하고 5,000명 이상의 지식 공유자, 140만 명 이상의 학습자가 함께 지식 생태계의 선순환을 만들고 있습니다.",
+            image: "/main/img/인프런.png",
+            url: "https://www.inflearn.com/",
+        },
+        {
+            name: "네이버 클라우드 플랫폼",
+            description:
+                "네이버 클라우드 예비 개발자 지원 프로그램 Green Developers 네이버 클라우드 플랫폼은 클라우드 스터디, 토이 프로젝트 등 자기 개발 목적으로 클라우드를 활용하는 예비, 주니어 개발자의 성장을 지원합니다.",
+            image: "/main/img/네이버클라우드.png",
+            url: "https://www.ncloud.com/",
+        },
+        {
+            name: "스나이퍼 팩토리",
+            description:
+                "스나이퍼 팩토리는 국내 유수의 IT 인재 육성을 위한 자체 부트캠프를 운영하며 IT 서비스 기획, UIUX 디자인, 앱/웹 개발자, AI, 빅데이터, 유니티 등 청년 IT 인재 육성을 키워나가고 있습니다.",
+            image: "/main/img/스나이퍼 팩토리.png",
+            url: "https://sniperfactory.com/",
+        },
+    ];
+
     return (
         <motion.div
             initial={{opacity: 0, y: 60}}
@@ -25,16 +51,40 @@ const PartnerSection = () => {
                 />
             </div>
             <p className={"mt-[48px] text-[20px]"}>KUSITMS과 함께하는 파트너사를 소개해요.</p>
-            <Image
-                className={"mt-[100px]"}
-                src="/main/img/파트너사.png"
-                alt="파트너사"
-                width={980}
-                height={300}
-                priority
-                style={{width: 980, height: 300}}
-            />
+            <section className="flex gap-x-[80px] mt-[100px]">
+                {partners.map((partner, idx) => (
+                    <PartnerCard key={idx} {...partner} />
+                ))}
+            </section>
         </motion.div>
     );
 };
 export default PartnerSection;
+
+const PartnerCard = ({name, description, image, url} : {
+    name: string;
+    description: string;
+    image: string;
+    url: string;
+    key: number;
+}) => (
+    <div className="relative p-[6px]">
+        <Link
+            href={url}
+            className="absolute top-0 right-0 flex flex-col items-center justify-center bg-black w-[82px] h-[82px] rounded-full"
+        >
+            <div className="flex flex-col items-center justify-center bg-[#0055FF] w-[66px] h-[66px] rounded-full">
+                <Image src="/main/화살표.svg" width={30} height={30} alt="화살표" />
+            </div>
+        </Link>
+        <div className="border w-[391px] h-[450px] bg-white rounded-[32px] m-[6px]">
+            <div className="flex flex-col items-center justify-center py-8 mx-8 border-b-[0.5px] border-black h-[249px]">
+                <Image src={image} width={235} height={165} alt={name} />
+            </div>
+            <div className="flex flex-col gap-y-3 p-8 h-[201px]">
+                <div className="text-[24px] text-[#05F] font-bold">{name}</div>
+                <div className="text-black text-[15px]">{description}</div>
+            </div>
+        </div>
+    </div>
+);
