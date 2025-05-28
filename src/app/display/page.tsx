de3b368f2e00e26bda04e063e5d83363f5e79dc9
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-28 21:51:51 +0900

    faet: 사진 등록

--- src/app/display/page.tsx
+++ src/app/display/page.tsx
@@ -20,7 +20,7 @@ async function DisplayPage() {
           큐밀리들의 31번째 여정
         </h2>
         <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
             OT
           </h3>
           <div className="flex items-center gap-x-5">
@@ -41,7 +41,7 @@ async function DisplayPage() {
           </div>
         </div>
         <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
             기업 프로젝트
           </h3>
           <div className="flex items-center gap-x-5">
@@ -69,68 +69,19 @@ async function DisplayPage() {
           </div>
         </div>
         <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            OT
+          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+            전문가 초청 강연
           </h3>
           <div className="flex items-center gap-x-5">
             <Image
-              src="/display/ot_1.png"
+              src="/display/invitation_1.png"
               alt="ot"
               width={640}
               height={360}
               className="w-[640px] h-[360px]"
             />
             <Image
-              src="/display/ot_2.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            기업 프로젝트
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/corporate_1.jpg"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/corporate_2.jpg"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/corporate_3.jpg"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            OT
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/ot_1.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-            <Image
-              src="/display/ot_2.png"
+              src="/display/invitation_2.png"
               alt="ot"
               width={640}
               height={360}
@@ -139,26 +90,75 @@ async function DisplayPage() {
           </div>
         </div>
         <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            기업 프로젝트
+          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+            MT
           </h3>
           <div className="flex items-center gap-x-5">
             <Image
-              src="/display/corporate_1.jpg"
+              src="/display/mt_1.png"
               alt="ot"
               width={381}
               height={381}
               className="w-[381px] h-[381px]"
             />
             <Image
-              src="/display/corporate_2.jpg"
+              src="/display/mt_2.png"
               alt="ot"
               width={381}
               height={381}
               className="w-[381px] h-[381px]"
             />
             <Image
-              src="/display/corporate_3.jpg"
+              src="/display/mt_3.png"
+              alt="ot"
+              width={381}
+              height={381}
+              className="w-[381px] h-[381px]"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
+          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+            OB 초청 데이
+          </h3>
+          <div className="flex items-center gap-x-5">
+            <Image
+              src="/display/ob_1.png"
+              alt="ot"
+              width={640}
+              height={360}
+              className="w-[640px] h-[360px]"
+            />
+            <Image
+              src="/display/ob_2.png"
+              alt="ot"
+              width={640}
+              height={360}
+              className="w-[640px] h-[360px]"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
+          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+            밋업 프로젝트
+          </h3>
+          <div className="flex items-center gap-x-5">
+            <Image
+              src="/display/meetup_1.png"
+              alt="ot"
+              width={381}
+              height={381}
+              className="w-[381px] h-[381px]"
+            />
+            <Image
+              src="/display/meetup_2.png"
+              alt="ot"
+              width={381}
+              height={381}
+              className="w-[381px] h-[381px]"
+            />
+            <Image
+              src="/display/meetup_3.png"
               alt="ot"
               width={381}
               height={381}
