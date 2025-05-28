8f61ee57f9c20215d094682142351f1079ae4067
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-05-28 21:56:14 +0900

    refactor: 사진 영역 리펙토링

--- src/app/display/page.tsx
+++ src/app/display/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { DisplayListContainer } from "@/components/display";
 import { getMeetupProjects } from "@/service/projects";
+import { DISPLAY_SECTIONS } from "@/constants/displayPictureList";
 
 async function DisplayPage() {
   const meetupProjectList = await getMeetupProjects("31", "");
@@ -19,153 +20,30 @@ async function DisplayPage() {
         <h2 className="text-5xl font-bold text-white text-center">
           큐밀리들의 31번째 여정
         </h2>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            OT
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/ot_1.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-            <Image
-              src="/display/ot_2.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            기업 프로젝트
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/corporate_1.jpg"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/corporate_2.jpg"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/corporate_3.jpg"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            전문가 초청 강연
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/invitation_1.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-            <Image
-              src="/display/invitation_2.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            MT
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/mt_1.png"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/mt_2.png"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/mt_3.png"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-3 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            OB 초청 데이
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/ob_1.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-            <Image
-              src="/display/ob_2.png"
-              alt="ot"
-              width={640}
-              height={360}
-              className="w-[640px] h-[360px]"
-            />
-          </div>
-        </div>
-        <div className="flex flex-col items-center gap-y-9 mt-[140px]">
-          <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
-            밋업 프로젝트
-          </h3>
-          <div className="flex items-center gap-x-5">
-            <Image
-              src="/display/meetup_1.png"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/meetup_2.png"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-            <Image
-              src="/display/meetup_3.png"
-              alt="ot"
-              width={381}
-              height={381}
-              className="w-[381px] h-[381px]"
-            />
-          </div>
-        </div>
+        <section className="w-full flex flex-col items-center">
+          {DISPLAY_SECTIONS.map((section, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center gap-y-9 mt-[140px]"
+            >
+              <h3 className="px-8 py-2 bg-[#05F] font-extrabold text-[28px] text-white w-[250px] mx-auto text-center rounded-full">
+                {section.title}
+              </h3>
+              <div className="flex items-center gap-x-5">
+                {section.images.map((image, imageIndex) => (
+                  <Image
+                    key={imageIndex}
+                    src={image.src}
+                    alt={section.title}
+                    width={image.width}
+                    height={image.height}
+                    className={image.className}
+                  />
+                ))}
+              </div>
+            </div>
+          ))}
+        </section>
       </section>
     </div>
   );
