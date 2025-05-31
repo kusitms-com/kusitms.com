2150e6c77200d1f49651c58956d81814ef66e36c
유림 <yulimh001@gmail.com>
2025-05-31 09:56:36 +0900

    fix: div -> p태그로 수정

--- src/app/display/[displayNumber]/page.tsx
+++ src/app/display/[displayNumber]/page.tsx
@@ -37,7 +37,7 @@ const DisplayDetailPage = async ({params}: Readonly<{
         <div className="flex gap-x-4 items-center">
             <div className="text-[18px] text-[#CCC] w-[78px]">{role}</div>
             <div className="h-[16px] border-r border-[#949494]" />
-            <div> {members.join(", ")}</div>
+            <p> {members.join(", ")}</p>
         </div>
     );
     
