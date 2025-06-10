cdb323e9c89d320e9363c12efac4bd7d85ec390f
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-06-11 00:57:31 +0900

    ♻️ Refactor: 정적으로 메타데이터 설정되게 변경
    refactor: 정적으로 메타데이터 설정되게 변경

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,56 +1,39 @@
 import { ReactNode } from "react";
+import { Metadata } from "next";
 
-interface Props {
-  params: Promise<{ projectType: string }>;
-  children: ReactNode;
-}
-
-export async function generateMetadata({ params }: Props) {
-  const { projectType } = await params;
-
-  return {
-    title: `KUSITMS | ${projectType === "meetup" ? "밋업" : "기업"} 프로젝트`,
+export const metadata: Metadata = {
+  title: "KUSITMS | Projects",
+  description: "큐시즘에서 진행한 다양한 프로젝트들을 한눈에 확인해보세요.",
+  keywords: ["큐시즘", "KUSITMS", "프로젝트", "밋업", "기업", "대학생 IT 학회"],
+  openGraph: {
+    title: "KUSITMS Projects",
     description: "큐시즘에서 진행한 다양한 프로젝트들을 한눈에 확인해보세요.",
-    keywords: [
-      "큐시즘",
-      "KUSITMS",
-      "프로젝트",
-      "밋업",
-      "기업",
-      "대학생 IT 학회",
-      projectType,
+    url: "https://www.kusitms.com/projects/meetup",
+    siteName: "KUSITMS",
+    images: [
+      {
+        url: "https://kusitms-bucket.s3.ap-northeast-2.amazonaws.com/upload/fdc0f561-9f61-47d0-be9b-1274cfe4814emarkup_banner.png",
+        alt: "KUSITMS 프로젝트 목록",
+      },
     ],
-    openGraph: {
-      title: `KUSITMS ${projectType === "meetup" ? "밋업" : "기업"} 프로젝트`,
-      description: "큐시즘에서 진행한 다양한 프로젝트들을 한눈에 확인해보세요.",
-      url: "https://www.kusitms.com/projects/meetup",
-      siteName: "KUSITMS",
-      images: [
-        {
-          url: "https://kusitms-bucket.s3.ap-northeast-2.amazonaws.com/upload/fdc0f561-9f61-47d0-be9b-1274cfe4814emarkup_banner.png",
-          alt: "KUSITMS 프로젝트 목록",
-        },
-      ],
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: `KUSITMS ${projectType === "meetup" ? "밋업" : "기업"} 프로젝트`,
-      description: "큐시즘에서 진행한 다양한 프로젝트들을 한눈에 확인해보세요.",
-      images: [
-        {
-          url: "https://kusitms-bucket.s3.ap-northeast-2.amazonaws.com/upload/fdc0f561-9f61-47d0-be9b-1274cfe4814emarkup_banner.png",
-          alt: "KUSITMS 프로젝트 목록",
-        },
-      ],
-    },
-  };
-}
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "KUSITMS Projects",
+    description: "큐시즘에서 진행한 다양한 프로젝트들을 한눈에 확인해보세요.",
+    images: [
+      {
+        url: "https://kusitms-bucket.s3.ap-northeast-2.amazonaws.com/upload/fdc0f561-9f61-47d0-be9b-1274cfe4814emarkup_banner.png",
+        alt: "KUSITMS 프로젝트 목록",
+      },
+    ],
+  },
+};
 
 export default async function ProjectsLayout({
   children,
 }: Readonly<{
   children: ReactNode;
-  params: Promise<{ projectType: string }>;
 }>) {
   return <main className="flex flex-col items-center">{children}</main>;
 }
