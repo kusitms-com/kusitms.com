f802125081eafa6709289e8c64cac37ee6543fed
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-10-19 16:31:33 +0900

    #145 [CHORE] Biome warn 변경

--- src/components/recruit/RecruitingTeamInfoItem.tsx
+++ src/components/recruit/RecruitingTeamInfoItem.tsx
@@ -1,3 +1,4 @@
+/** biome-ignore-all lint/security/noDangerouslySetInnerHtml: only inserting a controlled <br/>*/
 import React from "react";
 
 export default function RecruitingTeamInfoItem() {
@@ -8,16 +9,28 @@ export default function RecruitingTeamInfoItem() {
       }
     >
       <div className={"flex flex-col gap-y-[40px] items-center justify-center"}>
-        <div className={"flex flex-col desktop:flex-row gap-x-[100px] gap-y-[36px] items-center"}>
-          {["기획", "디자인", "개발<br/>(프론트엔드,백엔드)"].map((content, idx) => {
-            return (
-              <div
-                key={idx}
-                className={`${content == "디자인" ? "desktop:ml-35" : content === "개발<br/>(프론트엔드,백엔드)" ? "desktop:ml-10" : ""} font-extrabold text-[24px] leading-[120%] tracking-[0px] text-center`}
-                dangerouslySetInnerHTML={{ __html: content }}
-              />
-            );
-          })}
+        <div
+          className={
+            "flex flex-col desktop:flex-row gap-x-[100px] gap-y-[36px] items-center"
+          }
+        >
+          {["기획", "디자인", "개발<br/>(프론트엔드,백엔드)"].map(
+            (content, idx) => {
+              return (
+                <div
+                  key={idx}
+                  className={`${
+                    content === "디자인"
+                      ? "desktop:ml-35"
+                      : content === "개발<br/>(프론트엔드,백엔드)"
+                      ? "desktop:ml-10"
+                      : ""
+                  } font-extrabold text-[24px] leading-[120%] tracking-[0px] text-center`}
+                  dangerouslySetInnerHTML={{ __html: content }}
+                />
+              );
+            }
+          )}
         </div>
         <div className="flex flex-col gap-y-2">
           <p
