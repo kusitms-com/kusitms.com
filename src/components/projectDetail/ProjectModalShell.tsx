cc29f113c9c3d721ec9e0ae66c45fa09ffb87590
ahcgnoej <jinjinchachajung@naver.com>
2025-11-02 16:49:29 +0900

    #148 [STYLE] 모달 최대 높이 설정

--- src/components/projectDetail/ProjectModalShell.tsx
+++ src/components/projectDetail/ProjectModalShell.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-import { ReactNode } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import type { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
@@ -20,15 +20,10 @@ export default function ProjectModalShell({ children }: Props) {
         className="relative tablet:w-[624px] h-[90vh] mx-20 pb-4  bg-white rounded-xl flex flex-col"
         onClick={(e) => e.stopPropagation()}
       >
-        <div className="flex justify-between h-[88px] border-b px-10 items-center bg-white rounded-xl">
+        <div className="flex justify-between h-[88px] border-b px-10 items-center bg-white rounded-t-xl">
           <p className="text-title-7 text-gray-700">프로젝트 소개</p>
           <button onClick={() => router.back()}>
-            <Image
-              src="/projects/icons/CloseIcon2.svg"
-              alt="Close"
-              width={32}
-              height={32}
-            />
+            <Image src="/projects/icons/CloseIcon2.svg" alt="Close" width={32} height={32} />
           </button>
         </div>
         <div className="overflow-y-auto px-6 pt-4 flex-1">{children}</div>
