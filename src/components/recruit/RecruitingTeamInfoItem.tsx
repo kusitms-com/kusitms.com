2608b5c5f11f1b19287557cfaddd542b5510f855
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-06 03:15:25 +0900

    #153 [FEAT] 모집 파트 정보 컴포넌트

--- src/components/recruit/RecruitingTeamInfoItem.tsx
+++ src/components/recruit/RecruitingTeamInfoItem.tsx
@@ -1,52 +1,87 @@
-/** biome-ignore-all lint/security/noDangerouslySetInnerHtml: only inserting a controlled <br/>*/
-import React from "react";
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
 
 export default function RecruitingTeamInfoItem() {
+  const [selectedRole, setSelectedRole] = useState("기획");
+
+  const recruitInfoItems = [
+    {
+      role: "기획",
+      activity: ["기획 활동1", "기획 활동2", "기획 활동3"],
+      people: ["기획 인재상1", "기획 인재상2", "기획 인재상3"],
+    },
+    {
+      role: "디자인",
+      activity: ["디자인 활동1", "디자인 활동2", "디자인 활동3"],
+      people: ["디자인 인재상1", "디자인 인재상2", "디자인 인재상3"],
+    },
+    {
+      role: "프론트",
+      activity: ["프론트 활동1", "프론트 활동2", "프론트 활동3"],
+      people: ["프론트 인재상1", "프론트 인재상2", "프론트 인재상3"],
+    },
+    {
+      role: "백엔드",
+      activity: ["백엔드 활동1", "백엔드 활동2", "백엔드 활동3"],
+      people: ["백엔드 인재상1", "백엔드 인재상2", "백엔드 인재상3"],
+    },
+  ];
+
+  const selectedInfo = recruitInfoItems.find((item) => item.role === selectedRole);
+
   return (
-    <section
-      className={
-        "rounded-[32px] bg-[#2F3038] desktop:w-[1180px] w-[334px] desktop:py-[67px] pt-[60px] pb-[40px]"
-      }
-    >
-      <div className={"flex flex-col gap-y-[40px] items-center justify-center"}>
-        <div
-          className={
-            "flex flex-col desktop:flex-row gap-x-[100px] gap-y-[36px] items-center"
-          }
-        >
-          {["기획", "디자인", "개발<br/>(프론트엔드,백엔드)"].map(
-            (content, idx) => {
-              return (
-                <div
-                  key={idx}
-                  className={`${
-                    content === "디자인"
-                      ? "desktop:ml-35"
-                      : content === "개발<br/>(프론트엔드,백엔드)"
-                      ? "desktop:ml-10"
-                      : ""
-                  } font-extrabold text-[24px] leading-[120%] tracking-[0px] text-center`}
-                  dangerouslySetInnerHTML={{ __html: content }}
-                />
-              );
-            }
-          )}
+    <section className="flex flex-col gap-[70px] w-full">
+      {/* 파트 선택 네비게이션 */}
+      <div className="flex gap-[20px] justify-center">
+        {recruitInfoItems.map((item) => (
+          <button
+            key={item.role}
+            type="button"
+            onClick={() => setSelectedRole(item.role)}
+            className={`w-[120px] py-[10px] rounded-full text-body-2 
+              ${
+                selectedRole === item.role ? "bg-dark-blue-50 text-dark-blue-600" : "text-gray-700"
+              }`}
+          >
+            {item.role}
+          </button>
+        ))}
+      </div>
+
+      {/* 파트 정보 */}
+      <div className="flex flex-col gap-[60px]">
+        <div className="flex flex-col gap-[12px]">
+          <div className="flex gap-[8px] px-[4px] items-center">
+            <Image src="/recruit/icons/Union.svg" width={30} height={33} alt="아이콘" />
+            <h3 className="text-body-1 text-gray-900">{selectedRole} 파트는 이런 활동을 해요</h3>
+          </div>
+          <div className="flex flex-col gap-[16px] px-[28px] py-[24px] bg-gray-50 rounded-2xl">
+            {selectedInfo?.activity.map((activity, index) => (
+              <p key={index} className="flex">
+                <Image src="/recruit/icons/Dot.svg" width={30} height={30} alt="아이콘" />
+                <span className="text-gray-700 text-body-2">{activity}</span>
+              </p>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col gap-y-2">
-          <p
-            className={
-              "text-[#90909A] font-normal text-[16px] leading-[100%] tracking-[-0.5px] text-center"
-            }
-          >
-            * 팀별 인원 수는 매 기수 달라질 수 있어요 :)
-          </p>
-          <p
-            className={
-              "text-[#90909A] font-normal text-[16px] leading-[100%] tracking-[-0.5px] text-center"
-            }
-          >
-            * 운영진도 학회원 활동에 참여해요.
-          </p>
+      </div>
+
+      <div className="flex flex-col gap-[60px]">
+        <div className="flex flex-col gap-[12px]">
+          <div className="flex gap-[8px] px-[4px] items-center">
+            <Image src="/recruit/icons/Union.svg" width={30} height={33} alt="아이콘" />
+            <h3 className="text-body-1 text-gray-900">이런 분과 함께하고 싶어요</h3>
+          </div>
+          <div className="flex flex-col gap-[16px] px-[28px] py-[24px] bg-gray-50 rounded-2xl">
+            {selectedInfo?.people.map((person, index) => (
+              <span key={index} className="flex">
+                <Image src="/recruit/icons/Dot.svg" width={30} height={30} alt="아이콘" />
+                <p className="text-gray-700 text-body-2">{person}</p>
+              </span>
+            ))}
+          </div>
         </div>
       </div>
     </section>
