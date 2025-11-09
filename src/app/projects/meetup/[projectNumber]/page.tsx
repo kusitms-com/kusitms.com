81a108af2b0e4d3b9016576469f8b3ae3bc0a171
ahcgnoej <jinjinchachajung@naver.com>
2025-11-10 01:04:17 +0900

    #156 [FIX] 새로고침시에도 모달이 뜨돋록 수정

--- src/app/projects/meetup/[projectNumber]/page.tsx
+++ src/app/projects/meetup/[projectNumber]/page.tsx
@@ -1,6 +1,12 @@
+import ProjectModalShell from "@/components/projectDetail/ProjectModalShell";
+import ProjectNavigation from "@/components/projectDetail/ProjectNavigation";
+import NavButtons from "@/components/projects/common/NavButtons";
+import ProjectTypeBanner from "@/components/projects/common/ProjectTypeBanner";
+import ProjectContainer from "@/components/projects/MeetupProjectContainer";
 import OldProjectSection from "@/components/projects/OldProjectSection";
 import RecentProjectSection from "@/components/projects/RecentProjectSection";
 import { getMeetupProjectDetail, getMeetupProjects } from "@/service/projects";
+import { getAdjacentMeetupIds } from "@/utils";
 
 export async function generateStaticParams() {
   const meetupProjectList = await getMeetupProjects("");
@@ -15,18 +21,35 @@ async function ProjectDetailPage({
   params: Promise<{ projectNumber: string }>;
 }>) {
   const { projectNumber } = await params;
-  const { data: project } = await getMeetupProjectDetail(projectNumber);
 
-  // 31기 프로젝트 ID 리스트
+  const { data: project } = await getMeetupProjectDetail(projectNumber);
+  const projectList = await getMeetupProjects("");
+
   const recentIdList = [47, 50, 51, 53, 48, 55, 52, 49, 54];
+
+  const { prevId, nextId } = getAdjacentMeetupIds(projectList.data.meetup_list, projectNumber);
 
   return (
     <>
-      {recentIdList.includes(parseInt(projectNumber)) ? (
-        <RecentProjectSection project={project} />
-      ) : (
-        <OldProjectSection project={project} />
-      )}
+      <section className="w-full mx-auto max-w-[1024px]">
+        <NavButtons />
+        <ProjectTypeBanner
+          title={"밋업 프로젝트란?"}
+          detail={
+            "기획 파트에서 발제된 아이디어를 디자인, 개발 파트와 함께\n3개월 동안 준비해서 발표하는 큐시즘의 메인 프로젝트예요."
+          }
+        />
+        <ProjectContainer data={projectList.data} />
+      </section>
+
+      <ProjectModalShell>
+        <ProjectNavigation prevId={prevId} nextId={nextId} />
+        {recentIdList.includes(parseInt(projectNumber)) ? (
+          <RecentProjectSection project={project} />
+        ) : (
+          <OldProjectSection project={project} />
+        )}
+      </ProjectModalShell>
     </>
   );
 }
