7f9280bb34d94d1c32f95947d2502a9c81454b64
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-09 17:16:34 +0900

    #153 [FEAT] 모집 알림 입력창 추가

--- src/components/recruit/RecruitmentTogether.tsx
+++ src/components/recruit/RecruitmentTogether.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import MailInput from "./MailInput";
 
 const RecruitmentTogether = () => {
   // 모집기간 여부에 따라 문구와 버튼 다르게 처리
@@ -7,13 +8,14 @@ const RecruitmentTogether = () => {
   return (
     <div className="flex flex-col items-center">
       <h2 className="text-title-3 text-dark-blue-400 pb-[40px]">Recruitment</h2>
-      {isRecruiting ? (
+      {!isRecruiting ? (
         <>
           <span className="text-body-2 items-center flex flex-col text-gray-900 pb-[80px]">
             <p>큐시즘은 혼자서는 해낼 수 없는 일들을 함께 이루어내고 있어요.</p>
             <p>멋진 아이디어를 실현하고, 놀라운 결과를 만들어내요.</p>
             <p>함께 큐밀리가 되시겠어요?</p>
           </span>
+          {/* TODO: 실제 지원 링크로 이동 */}
           <button className="text-gray-0 rounded-full text-body-4 flex gap-[12px] justify-center items-center px-[24px] py-[10px] bg-dark-blue-500">
             <p>함께하러 가기</p>
             <Image
@@ -31,10 +33,7 @@ const RecruitmentTogether = () => {
             <p>지금은 모집 기간이 아니에요</p>
             <p>모집 기간이 되면 메일로 알려드릴게요</p>
           </span>
-          <button className="text-gray-0 rounded-full text-body-4 flex gap-[12px] justify-center items-center px-[24px] py-[10px] bg-dark-blue-500">
-            <p>함께하러 가기</p>
-            <Image src="/recruit/icons/WhiteArrow.svg" alt="Arrow" width={24} height={24} />
-          </button>
+          <MailInput />
         </>
       )}
     </div>
