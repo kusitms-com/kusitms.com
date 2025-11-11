2231784b2c2a4843a61ef78dcbf2eaa7680915f1
sunhwaaRj <pickle02@sookmyung.ac.kr>
2025-11-11 17:32:44 +0900

    #159 [CHORE] 네비게이션바 url 변경

--- src/components/shared/PageNav.tsx
+++ src/components/shared/PageNav.tsx
@@ -8,7 +8,7 @@ const navLinks = [
   { href: "/", label: "학회소개", matchPath: "/" },
   { href: "/projects/meetup", label: "프로젝트", matchPath: "/projects" },
   { href: "/archive", label: "32기 아카이브", matchPath: "/archive" },
-  { href: "/reviews", label: "후기", matchPath: "/reviews" },
+  { href: "/reviews/activity", label: "후기", matchPath: "/reviews" },
   { href: "/recruit", label: "리크루팅", matchPath: "/recruit" },
 ];
 
