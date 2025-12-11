005b526361e0d280334a6ec6c5e981a7d7e07e51
parknari02 <pnr021118@naver.com>
2025-12-12 01:25:40 +0900

    [FEAT] : 배너 반응형 적용

--- src/components/main/IntroSection.tsx
+++ src/components/main/IntroSection.tsx
@@ -10,51 +10,99 @@ const OUTLINE_TEXT_BASE = `${OUTFIT_FONT} text-[#0077FF] leading-[100%] -trackin
 export default function IntroSection() {
   return (
     <div
-      className="relative w-full h-[808px] desktop:mt-[74px]"
+      className="relative w-full h-[400px] min-[768px]:h-[808px] desktop:mt-[74px] overflow-hidden"
       style={{ background: BACKGROUND_GRADIENT }}
     >
-      <p className="absolute z-10 top-[72px] right-[75px] text-body-6 text-[#0077FF] text-right">
-        Korean University Students
-        <br />
-        It, Management Society
-      </p>
-      <motion.div
-        className="absolute z-10 top-[72px] left-[84px] space-y-4"
-        initial={{ y: -50, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-        transition={{
-          y: {
-            duration: 2,
-            ease: "easeOut",
-          },
-          opacity: {
-            duration: 2,
-            ease: "easeOut",
-          },
-        }}
-      >
-        <motion.div
-          animate={{ y: [0, -8, 0] }}
-          transition={{ y: { duration: 3, repeat: 4, ease: "easeInOut", delay: 2 } }}
-        >
-          <p className={`${OUTLINE_TEXT_BASE} text-[42px]`}>KUSITMS 32nd</p>
-          <div className="flex items-center gap-x-9">
-            <p className={`${OUTLINE_TEXT_BASE} text-[68px]`}>More it, MORE IT</p>
+      <div className="max-[767px]:block min-[768px]:hidden relative z-10 flex flex-col items-center justify-center h-full">
+        <div className="flex flex-col px-9">
+          <motion.div
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              y: {
+                duration: 2,
+                ease: "easeOut",
+              },
+              opacity: {
+                duration: 2,
+                ease: "easeOut",
+              },
+            }}
+          >
             <motion.div
-              animate={{ rotate: 360 }}
-              transition={{
-                rotate: {
-                  duration: 3,
-                  repeat: Infinity,
-                  ease: "linear",
-                },
-              }}
+              animate={{ y: [0, -8, 0] }}
+              transition={{ y: { duration: 3, repeat: 4, ease: "easeInOut", delay: 2 } }}
+              className="flex flex-col gap-1 mt-8"
             >
-              <Image src="/main/Snow.svg" alt="Snow" width={54} height={59} />
+              <p className={`${OUTLINE_TEXT_BASE} text-[18px]`}>KUSITMS 32nd</p>
+              <div className="flex items-center gap-x-2">
+                <p className={`${OUTLINE_TEXT_BASE} text-[28px]`}>More it, MORE IT</p>
+                <motion.div
+                  animate={{ rotate: 360 }}
+                  transition={{
+                    rotate: {
+                      duration: 6,
+                      repeat: Infinity,
+                      ease: "linear",
+                    },
+                  }}
+                >
+                  <Image src="/main/Snow.svg" alt="Snow" width={20} height={20} />
+                </motion.div>
+              </div>
             </motion.div>
-          </div>
+          </motion.div>
+          <p className="text-body-8 text-[#0077FF] pt-6">
+            Korean University Students
+            <br />
+            It, Management Society
+          </p>
+        </div>
+      </div>
+      <div className="hidden min-[768px]:block">
+        <p className="absolute z-10 top-[72px] right-[75px] text-body-6 text-[#0077FF] text-right">
+          Korean University Students
+          <br />
+          It, Management Society
+        </p>
+        <motion.div
+          className="absolute z-10 top-[72px] left-[84px] space-y-4"
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{
+            y: {
+              duration: 2,
+              ease: "easeOut",
+            },
+            opacity: {
+              duration: 2,
+              ease: "easeOut",
+            },
+          }}
+        >
+          <motion.div
+            animate={{ y: [0, -8, 0] }}
+            transition={{ y: { duration: 3, repeat: 4, ease: "easeInOut", delay: 2 } }}
+          >
+            <p className={`${OUTLINE_TEXT_BASE} text-[42px]`}>KUSITMS 32nd</p>
+            <div className="flex items-center gap-x-9">
+              <p className={`${OUTLINE_TEXT_BASE} text-[68px]`}>More it, MORE IT</p>
+              <motion.div
+                animate={{ rotate: 360 }}
+                transition={{
+                  rotate: {
+                    duration: 3,
+                    repeat: Infinity,
+                    ease: "linear",
+                  },
+                }}
+              >
+                <Image src="/main/Snow.svg" alt="Snow" width={54} height={59} />
+              </motion.div>
+            </div>
+          </motion.div>
         </motion.div>
-      </motion.div>
+      </div>
       <motion.div
         className="absolute inset-0 z-0"
         initial={{ y: -30, opacity: 0 }}
@@ -82,12 +130,14 @@ export default function IntroSection() {
             },
           }}
         >
-          <Image
-            src="/main/img/Main_Graphic.png"
-            alt="Main Graphic"
-            fill
-            className="object-cover object-center"
-          />
+          <div className="absolute top-0 bottom-0 left-0 max-[767px]:right-[-40px] min-[768px]:right-0 max-[767px]:-translate-x-[40px] min-[768px]:translate-y-0 min-[768px]:translate-x-0">
+            <Image
+              src="/main/img/Main_Graphic.png"
+              alt="Main Graphic"
+              fill
+              className="object-cover object-center"
+            />
+          </div>
         </motion.div>
       </motion.div>
     </div>
