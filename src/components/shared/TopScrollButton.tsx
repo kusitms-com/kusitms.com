70a4882ee8ca947f677e68d851b111812acf3ec8
YuminPark <p353700@gmail.com>
2026-06-04 18:42:53 +0900

    refator: TopScrollButton 디자인 시스템 ScrollTopButton으로 교체

--- src/components/shared/TopScrollButton.tsx
+++ src/components/shared/TopScrollButton.tsx
@@ -1,16 +1,13 @@
 "use client";
 
-import Image from "next/image";
+import { ScrollTopButton as ScrollTopButtonUI } from "@kusitms.com/ui";
 import { useEffect, useState } from "react";
-
-// 이걸로 통일. 기존 top버튼 모두 제거
 
 function TopScrollButton() {
   const [bottom, setBottom] = useState(40);
   const [isDesktop, setIsDesktop] = useState(true);
 
   useEffect(() => {
-    // 뷰포트 크기 체크
     const checkIsDesktop = () => {
       setIsDesktop(window.innerWidth >= 1024);
     };
@@ -24,9 +21,7 @@ function TopScrollButton() {
       const docHeight = document.documentElement.scrollHeight;
       const scrollY = window.scrollY || window.pageYOffset;
       const winHeight = window.innerHeight;
-      // 바닥까지 남은 거리
       const remain = docHeight - (scrollY + winHeight);
-      // 하단 고정값
       const minBottom = isDesktop ? 177 : 226;
       const defaultBottom = isDesktop ? 40 : 16;
 
@@ -43,10 +38,7 @@ function TopScrollButton() {
   }, [isDesktop]);
 
   const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   const style: React.CSSProperties = {
@@ -56,22 +48,7 @@ function TopScrollButton() {
     zIndex: 50,
   };
 
-  return (
-    <button
-      onClick={scrollToTop}
-      style={style}
-      className="cursor-pointer"
-      aria-label="맨 위로 이동"
-    >
-      <Image
-        src="/topScroll.svg"
-        alt="Top Scroll"
-        width={48}
-        height={48}
-        className="desktop:w-12 desktop:h-12 w-10 h-10"
-      />
-    </button>
-  );
+  return <ScrollTopButtonUI onClick={scrollToTop} style={style} />;
 }
 
 export default TopScrollButton;
